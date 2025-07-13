8ac592624d060633043b509f7377a57bbfe11130
Ronaldo-Junior1 <ronaldosjr2005@gmail.com>
2025-07-12 23:04:10 -0300

    Implementação do service de Emprestimo para se comunicar corretamente com o repository

--- src/service/EmprestimoService.ts
+++ src/service/EmprestimoService.ts
@@ -1,106 +1,105 @@
-// import { EmprestimoEntity } from "../model/entity/EmprestimoEntity";
-// import { EmprestimoRepository } from "../repository/EmprestimoRepository";
-// import { UsuarioRepository } from "../repository/UsuarioRepository";
-// import { EstoqueService } from "./EstoqueService";
-// import { LivroRepository } from "../repository/LivroRepository";
-// import { UsuarioService } from "./UsuarioService";
+import { EmprestimoEntity } from "../model/entity/EmprestimoEntity";
+import { StatusUsuario } from "../model/enum/StatusUsuario";
+import { EmprestimoRepository } from "../repository/EmprestimoRepository";
+import { LivroRepository } from "../repository/LivroRepository";
+import { UsuarioRepository } from "../repository/UsuarioRepository";
+import { EstoqueService } from "./EstoqueService";
+import { UsuarioService } from "./UsuarioService";
 
-// export class EmprestimoService {
-//     private emprestimoRepository = EmprestimoRepository.getInstance();
-//     private usuarioRepository = UsuarioRepository.getInstance();
-//     private livroRepository = LivroRepository.getInstance();
-//     private estoqueService = new EstoqueService();
-//     private usuarioService = new UsuarioService();
+export class EmprestimoService {
+    private emprestimoRepository = EmprestimoRepository.getInstance();
+    private usuarioRepository = UsuarioRepository.getInstance();
+    private livroRepository = LivroRepository.getInstance();
+    private estoqueService = new EstoqueService();
+    private usuarioService = new UsuarioService();
 
-//     novoEmprestimo(data: { cpf: string, codigo_exemplar: number }): EmprestimoEntity {
-//         const { cpf, codigo_exemplar } = data;
-        
-//         const usuario = this.usuarioRepository.findByCPF(cpf);
-//         if (!usuario) throw new Error("Usuário não encontrado.");
+    async novoEmprestimo(data: { cpf: string, codigo_exemplar: number }): Promise<EmprestimoEntity> {
+        const { cpf, codigo_exemplar } = data;
 
-//         const estoque = this.estoqueService.buscarPorCodigo(codigo_exemplar);
-        
-//         if (usuario.ativo !== "ativo") {
-//             throw new Error(`Usuário está com status '${usuario.ativo}' e não pode realizar empréstimos.`);
-//         }
+        const usuario = await this.usuarioRepository.filterUsuarioByCpf(cpf);
+        const estoque = await this.estoqueService.buscarPorCodigo(codigo_exemplar);
 
-//         if (!estoque.disponivel) {
-//             throw new Error("Este exemplar não está disponível.");
-//         }
+        if (usuario.status !== StatusUsuario.ATIVO) {
+            throw new Error(`Usuário está com status '${usuario.status}' e não pode realizar empréstimos.`);
+        }
 
-//         const emprestimosAtivos = this.emprestimoRepository.findAtivosByUsuarioId(usuario.id);
-//         const limite = usuario.categoria_id === 1 ? 5 : 3; // Prof = 5, Aluno = 3 
-        
-//         if (emprestimosAtivos.length >= limite) {
-//             throw new Error("Limite de empréstimos atingido para esta categoria de usuário.");
-//         }
+        if (!estoque.disponivel) {
+            throw new Error("Este exemplar não está disponível para empréstimo.");
+        }
 
-//         let diasEmprestimo = usuario.categoria_id === 1 ? 40 : 15;
-//         const livro = this.livroRepository.findById(estoque.livro_id);
-//         // if (usuario.categoria_id === 2 && livro && usuario.curso_id === livro.categoria_id) {
-//         //     diasEmprestimo = 30;
-//         // }
-//         const dataDevolucao = new Date();
-//         dataDevolucao.setDate(dataDevolucao.getDate() + diasEmprestimo);
-        
+        const emprestimosAtivos = await this.emprestimoRepository.findAtivosByUsuarioId(usuario.id!);
 
-//         const novoEmprestimo = new EmprestimoEntity(usuario.id, estoque.id, dataDevolucao);
-//         this.emprestimoRepository.registraEmprestimo    (novoEmprestimo);
+        const limite = usuario.categoria_id === 1 ? 5 : 3; // Professor: 5, Aluno: 3
+        if (emprestimosAtivos.length >= limite) {
+            throw new Error("Limite de empréstimos atingido para esta categoria de usuário.");
+        }
 
-//         this.estoqueService.registrarEmprestimo(codigo_exemplar);
-        
-//         return novoEmprestimo;
-//     }
+        let diasEmprestimo = usuario.categoria_id === 1 ? 40 : 15; // Professor: 40, Aluno: 15
+        const livro = await this.livroRepository.findById(estoque.livro_id);
 
-//     listarEmprestimos() {
-//         return this.emprestimoRepository.findAll();
-//     }
+        if (usuario.categoria_id === 2 && usuario.curso_id === livro.categoria_id) {
+            diasEmprestimo = 30;
+        }
 
-//     registrarDevolucao(emprestimoId: number): EmprestimoEntity {
-//         const emprestimo = this.emprestimoRepository.findById(emprestimoId);
-//         if (!emprestimo) throw new Error("Empréstimo não encontrado.");
-//         if (emprestimo.data_entrega) throw new Error("Este livro já foi devolvido.");
+        const dataDevolucao = new Date();
+        dataDevolucao.setDate(dataDevolucao.getDate() + diasEmprestimo);
+        const novoEmprestimo = new EmprestimoEntity(usuario.id!, estoque.id, dataDevolucao);
+        const emprestimoRegistrado = await this.emprestimoRepository.insertEmprestimo(novoEmprestimo);
 
-//         emprestimo.data_entrega = new Date();
-//         emprestimo.dias_atraso = this.calcularDiasDeAtraso(emprestimo.data_entrega, emprestimo.data_devolucao);
+        await this.estoqueService.registrarEmprestimo(codigo_exemplar);
 
-//         if (emprestimo.dias_atraso > 0) {
-//             const usuario = this.usuarioRepository.findById(emprestimo.usuario_id);
-//             if (usuario) {
-//                 const diasDeSuspensao = emprestimo.dias_atraso * 3;
-//                 const dataSuspensaoFinal = new Date();
-//                 dataSuspensaoFinal.setDate(dataSuspensaoFinal.getDate() + diasDeSuspensao);
-//                 emprestimo.suspensao_ate = dataSuspensaoFinal;
+        console.log("Service - Novo empréstimo registrado:", emprestimoRegistrado);
+        return emprestimoRegistrado;
+    }
 
-//                 const totalEmprestimosComAtraso = this.emprestimoRepository.countEmprestimosComAtrasoPorUsuario(usuario.id);
+    async listarEmprestimos(): Promise<EmprestimoEntity[]> {
+        const emprestimos = await this.emprestimoRepository.findAll();
+        console.log("Service - Listando todos os empréstimos:", emprestimos);
+        return emprestimos;
+    }
 
-//                 if (totalEmprestimosComAtraso > 2 && diasDeSuspensao > 60) {
-//                     this.usuarioService.inativarUsuario(usuario.cpf);
-//                 } else if (diasDeSuspensao > 60) {
-//                     this.usuarioService.aplicarSuspensao(usuario.cpf);
-//                 }
-//             }
-//         }
+    async registrarDevolucao(emprestimoId: number): Promise<EmprestimoEntity> {
+        const emprestimo = await this.emprestimoRepository.findById(emprestimoId);
+        if (emprestimo.data_entrega) {
+            throw new Error("Este livro já foi devolvido.");
+        }
 
-//         this.estoqueService.registrarDevolucao(emprestimo.estoque_id);
-//         return emprestimo;
-//     }
+        emprestimo.data_entrega = new Date();
+        emprestimo.dias_atraso = this.calcularDiasDeAtraso(emprestimo.data_entrega, new Date(emprestimo.data_devolucao));
 
-//     private calcularDiasDeAtraso(dataEntrega: Date, dataDevolucao: Date): number {
-//         const entregaNormalizada = new Date(dataEntrega);
-//         entregaNormalizada.setHours(0, 0, 0, 0);
+        if (emprestimo.dias_atraso > 0) {
+            const diasDeSuspensao = emprestimo.dias_atraso * 3;
+            const dataSuspensaoFinal = new Date();
+            dataSuspensaoFinal.setDate(dataSuspensaoFinal.getDate() + diasDeSuspensao);
+            emprestimo.suspensao_ate = dataSuspensaoFinal;
+            const usuario = await this.usuarioRepository.filterUsuarioById(emprestimo.usuario_id);
+            const totalAtrasos = await this.emprestimoRepository.countEmprestimosComAtrasoPorUsuario(usuario.id!);
 
-//         const devolucaoNormalizada = new Date(dataDevolucao);
-//         devolucaoNormalizada.setHours(0, 0, 0, 0);
+            if (totalAtrasos > 2) {
+                await this.usuarioService.alterarStatus(usuario.cpf, StatusUsuario.INATIVO);
+            } else if (diasDeSuspensao > 60) {
+                await this.usuarioService.alterarStatus(usuario.cpf, StatusUsuario.SUSPENSO);
+            }
+        }
+        const emprestimoAtualizado = await this.emprestimoRepository.updateEmprestimo(emprestimo);
 
-//         if (entregaNormalizada <= devolucaoNormalizada) {
-//             return 0;
-//         }
+        await this.estoqueService.registrarDevolucao(emprestimo.estoque_id);
 
-//         const umDiaEmMs = 1000 * 60 * 60 * 24;
-//         const diferencaEmMs = entregaNormalizada.getTime() - devolucaoNormalizada.getTime();
+        console.log("Service - Devolução registrada:", emprestimoAtualizado);
+        return emprestimoAtualizado;
+    }
 
-//         return diferencaEmMs / umDiaEmMs;
-//     }
+    private calcularDiasDeAtraso(dataEntrega: Date, dataDevolucao: Date): number {
+        const entrega = new Date(dataEntrega.getFullYear(), dataEntrega.getMonth(), dataEntrega.getDate());
+        const devolucao = new Date(dataDevolucao.getFullYear(), dataDevolucao.getMonth(), dataDevolucao.getDate());
 
-// }
+        if (entrega <= devolucao) {
+            return 0;
+        }
+
+        const umDiaEmMs = 1000 * 60 * 60 * 24;
+        const diferencaEmMs = entrega.getTime() - devolucao.getTime();
+
+        return Math.ceil(diferencaEmMs / umDiaEmMs);
+    }
+}
